e8221008d1f2fcdd6a987a0846bb7bd68ee42953
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-06-14 18:09:42 +0500

    pw protected view and user only view added with templates

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -15,13 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view, about_view
+from .views import (
+        home_view, 
+        about_view, 
+        pw_protected_view,
+        user_only_view
+)
 from auth.views import login_view, register_view
 urlpatterns = [
     path ('', home_view, name='home'),
     path('login/', login_view),
     path('register/', register_view),
     path('about/', about_view),
-    path('accounts/', include('allauth.urls')),    
+    path('accounts/', include('allauth.urls')),
+    path('protected/user-only/', user_only_view),
+    path('protected/', pw_protected_view),    
     path('admin/', admin.site.urls),
 ]
--- cfehome/views.py
+++ cfehome/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from visits.models import PageVisit
 
 def home_view(request, *args, **kwargs):
@@ -24,3 +25,20 @@ def about_view(request, *args, **kwargs):
     html_template = "home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, context)
+
+VALID_CODE = "abc123"
+
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get("protected_page_allowed") or 0
+    if request.method == "POST":
+        user_pw_sent = request.POST.get("code") or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session['proctected_page_allowed'] = is_allowed
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html", {})
