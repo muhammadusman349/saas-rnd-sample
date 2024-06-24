d6fe52b0f40908159916493b3c1667ae921d51a4
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-06-24 15:52:07 +0500

    profile app added

--- cfehome/urls.py
+++ cfehome/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('protected/user-only/', user_only_view),
     path('protected/staff-only/', staff_only_view),
-    path('protected/', pw_protected_view),    
+    path('protected/', pw_protected_view),
+    path('profiles/', include('profiles.urls')),    
     path('admin/', admin.site.urls),
 ]
---
+++ profiles/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.profile_list_view),
+    path ('<str:username>/', views.profile_view),
+]
