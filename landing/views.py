eb4d9ed4a691dab8a50438555abcdf75181976cf
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-07-10 13:08:07 +0500

    final dashboard added with template

--- landing/views.py
+++ landing/views.py
@@ -1,10 +1,12 @@
 import helpers.numbers
 from django.shortcuts import render
 from visits.models import PageVisit
-
+from dashboard.views import dashboard_view
 # Create your views here.
 
-def landing_page_view(request):
+def landing_dashboard_page_view(request):
+    if request.user.is_authenticated:
+        return dashboard_view(request)
     qs = PageVisit.objects.all()
     PageVisit.objects.create(path=request.path)
     page_views_formatted = helpers.numbers.shorten_number(qs.count() * 100_000)
